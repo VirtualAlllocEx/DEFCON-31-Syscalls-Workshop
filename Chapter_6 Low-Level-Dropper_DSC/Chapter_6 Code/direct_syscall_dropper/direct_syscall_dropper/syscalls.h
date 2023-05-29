69fa4f0bb30bb8d5a602ee329a94d086a82ead63
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-05-29 20:29:39 +0200

    Add files via upload

---
+++ Chapter_6 Low-Level-Dropper_DSC/Chapter_6 Code/direct_syscall_dropper/direct_syscall_dropper/syscalls.h
@@ -0,0 +1,60 @@
+#ifndef _SYSCALLS_H  // If _SYSCALLS_H is not defined then define it and the contents below. This is to prevent double inclusion.
+#define _SYSCALLS_H  // Define _SYSCALLS_H
+
+#include <windows.h>  // Include the Windows API header
+
+#ifdef __cplusplus   // If this header file is included in a C++ file, then this section will be true
+extern "C" {         // This is to ensure that the names of the functions are not mangled by the C++ compiler and are in C linkage format
+#endif
+
+    // The type NTSTATUS is typically defined in the Windows headers as a long.
+    typedef long NTSTATUS;  // Define NTSTATUS as a long
+    typedef NTSTATUS* PNTSTATUS;  // Define a pointer to NTSTATUS
+
+    // Declare the function prototype for NtAllocateVirtualMemory
+    extern NTSTATUS NtAllocateVirtualMemory(
+        HANDLE ProcessHandle,    // Handle to the process in which to allocate the memory
+        PVOID* BaseAddress,      // Pointer to the base address
+        ULONG_PTR ZeroBits,      // Number of high-order address bits that must be zero in the base address of the section view
+        PSIZE_T RegionSize,      // Pointer to the size of the region
+        ULONG AllocationType,    // Type of allocation
+        ULONG Protect            // Memory protection for the region of pages
+    );
+
+    // Declare the function prototype for NtWriteVirtualMemory
+    extern NTSTATUS NtWriteVirtualMemory(
+        HANDLE ProcessHandle,     // Handle to the process in which to write the memory
+        PVOID BaseAddress,        // Pointer to the base address
+        PVOID Buffer,             // Buffer containing data to be written
+        SIZE_T NumberOfBytesToWrite, // Number of bytes to be written
+        PULONG NumberOfBytesWritten // Pointer to the variable that receives the number of bytes written
+    );
+
+    // Declare the function prototype for NtCreateThreadEx
+    extern NTSTATUS NtCreateThreadEx(
+        PHANDLE ThreadHandle,        // Pointer to a variable that receives a handle to the new thread
+        ACCESS_MASK DesiredAccess,   // Desired access to the thread
+        PVOID ObjectAttributes,      // Pointer to an OBJECT_ATTRIBUTES structure that specifies the object's attributes
+        HANDLE ProcessHandle,        // Handle to the process in which the thread is to be created
+        PVOID lpStartAddress,        // Pointer to the application-defined function of type LPTHREAD_START_ROUTINE to be executed by the thread
+        PVOID lpParameter,           // Pointer to a variable to be passed to the thread
+        ULONG Flags,                 // Flags that control the creation of the thread
+        SIZE_T StackZeroBits,        // A pointer to a variable that specifies the number of high-order address bits that must be zero in the stack pointer
+        SIZE_T SizeOfStackCommit,    // The size of the stack that must be committed at thread creation
+        SIZE_T SizeOfStackReserve,   // The size of the stack that must be reserved at thread creation
+        PVOID lpBytesBuffer          // Pointer to a variable that receives any output data from the system
+    );
+
+    // Declare the function prototype for NtWaitForSingleObject
+    extern NTSTATUS NtWaitForSingleObject(
+        HANDLE Handle,          // Handle to the object to be waited on
+        BOOLEAN Alertable,      // If set to TRUE, the function returns when the system queues an I/O completion routine or APC for the thread
+        PLARGE_INTEGER Timeout  // Pointer to a LARGE_INTEGER that specifies the absolute```c
+        // or relative time at which the function should return, regardless of the state of the object
+    );
+
+#ifdef __cplusplus  // End of the 'extern "C"' block if __cplusplus was defined
+}
+#endif
+
+#endif // _SYSCALLS_H  // End of the _SYSCALLS_H definition
