2c4022d35739f41daa8ee997619ba052d4d0edde
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-06-01 15:14:51 +0200

    Add files via upload

---
+++ Chapter_7 Indirect-Syscall-Dropper/Chapter_7 Code/Indirect-Syscall-Dropper/Indirect-Syscall-Dropper/syscalls.asm
@@ -0,0 +1,18 @@
+EXTERN sysAddrNtAllocateVirtualMemory:QWORD         ; The actual address of the NtAllocateVirtualMemory syscall in ntdll.dll.
+
+; something is missing here;-)
+
+
+.CODE  ; Start the code section
+
+; Procedure for the NtAllocateVirtualMemory syscall
+NtAllocateVirtualMemory PROC
+    mov r10, rcx                                    ; Move the contents of rcx to r10. This is necessary because the syscall instruction in 64-bit Windows expects the parameters to be in the r10 and rdx registers.
+    mov eax, 18h                                    ; Move the syscall number into the eax register.
+    jmp QWORD PTR [sysAddrNtAllocateVirtualMemory]  ; Jump to the actual syscall.
+NtAllocateVirtualMemory ENDP                     	; End of the procedure.
+
+
+; something is missing here;-)
+
+END  ; End of the module
