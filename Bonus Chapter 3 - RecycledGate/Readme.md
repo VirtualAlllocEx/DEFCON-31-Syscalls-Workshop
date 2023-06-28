8389113706757d38009421951533247e99dc9dc3
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-06-28 15:41:16 +0200

    Delete Bonus Chapter 3 - RecycledGate directory

--- Bonus Chapter 3 - RecycledGate/Readme.md
+++
@@ -1,5 +0,0 @@
-## RecycledGate POC
-
-For this exercise we use the POC from [@thefLinkk](https://twitter.com/thefLinkk), **all creds to him!** 
-
-- https://github.com/thefLink/RecycledGate
--- Bonus Chapter 3 - RecycledGate/RecycledGate-main/Sample/Defines.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "windows.h"
-
-typedef VOID(KNORMAL_ROUTINE) (
-	IN PVOID NormalContext,
-	IN PVOID SystemArgument1,
-	IN PVOID SystemArgument2);
-
-typedef KNORMAL_ROUTINE* PKNORMAL_ROUTINE;
