7c29a0080c5f375c09e9ac8e0aced4d2edecaf2c
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2023-09-06 16:53:13 +0200

    Update syscalls.asm

--- 03_Chapter_3_POC/DSC-Loader/DSC-Loader/syscalls.asm
+++ 03_Chapter_3_POC/DSC-Loader/DSC-Loader/syscalls.asm
@@ -4,7 +4,7 @@
 ; Procedure for the NtAllocateVirtualMemory syscall
 NtAllocateVirtualMemory PROC
     mov r10, rcx                                    ; Move the contents of rcx to r10. This is necessary because the syscall instruction in 64-bit Windows expects the parameters to be in the r10 and rdx registers.
-    mov eax, 18                                    ; Move the syscall number into the eax register.
+    mov eax, 18h                                    ; Move the syscall number into the eax register.
     syscall                                         ; Execute syscall.
     ret                                             ; Return from the procedure.
 NtAllocateVirtualMemory ENDP                     	; End of the procedure.
